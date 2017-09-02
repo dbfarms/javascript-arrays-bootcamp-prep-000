8b422a54529d3db65314af74348aa1c1cf53eec5
Matthew Hunger <matt.hunger@gmail.com>
2017-09-02 00:51:21 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+var nerds = "nerds"
+
+function addElementToBeginningOfArray(chocolateBars, nerds) {
+  return [nerds, ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, nerds) {
+  chocolateBars.unshift(nerds)
+  return chocolateBars
+}
+
+function addElementToEndOfArray(chocolateBars, nerds) {
+  return [...chocolateBars, nerds]
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, nerds) {
+    chocolateBars.push(nerds)
+    return chocolateBars
+}
+
+function accessElementInArray(chocolateBars, 0) {}
+  return chocolateBars[0]
+}
